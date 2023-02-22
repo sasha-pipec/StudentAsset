3a8944857ba34195757a7b7a3dfd1cfabc8617d6
SomeBody <maxim.karaichev@saltpepper.ru>
2023-02-23 00:24:33 +0300

    added new data in admin user

--- models_app/admin/user/resources.py
+++ models_app/admin/user/resources.py
@@ -5,18 +5,20 @@ from models_app.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    fields = [
-        "username",
-        "API_Key",
-        "group",
-        "role",
-        "is_muted",
-        "created_at",
-        "updated_at",
+    fieldsets = [
+        ("Общая информация", {'fields': ['username', 'password', 'group', 'role', 'is_muted', 'API_Key']}),
+        ('Права доступа', {'fields': ['is_superuser', 'user_permissions', 'groups', 'is_staff', 'is_active']}),
+        ('Прочая информация',
+         {'fields': ['last_login', 'date_joined', 'first_name', 'last_name', 'created_at', 'updated_at']}),
     ]
+    save_on_top = True
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
     list_display = [
         "id",
         "username",
+        "group",
+        "role",
+        "is_muted",
     ]
     readonly_fields = ["id", "created_at", "updated_at"]
     list_display_links = (
