4d6e0ff19773eebcd46e0b1b3a47393e9635b7ac
SomeBody <maxim.karaichev@saltpepper.ru>
2023-02-22 20:51:49 +0300

    added migrate

--- models_app/models/__init__.py
+++ models_app/models/__init__.py
@@ -3,3 +3,6 @@ from models_app.models.event.models import Event
 from models_app.models.selection.models import Selection
 from models_app.models.thread.models import Thread
 from models_app.models.vote.models import Vote
+from models_app.models.like.models import Like
+from models_app.models.post.models import Post
+from models_app.models.comment.models import Comment
