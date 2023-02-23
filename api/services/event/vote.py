be4b90a8543876a13ad8dce7f6e19a855884ce17
SomeBody <maxim.karaichev@saltpepper.ru>
2023-02-23 14:04:51 +0300

    fix validation

--- api/services/event/vote.py
+++ api/services/event/vote.py
@@ -1,9 +1,7 @@
 from functools import lru_cache
 
 from django import forms
-from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
-from service_objects.errors import NotFound
 from service_objects.errors import ValidationError
 from service_objects.fields import ModelField
 from service_objects.services import ServiceWithResult
@@ -18,7 +16,7 @@ class EventVoteServices(ServiceWithResult):
     id = forms.IntegerField()
     user = ModelField(User)
 
-    custom_validations = ["selection_presence", "vote_presence"]
+    custom_validations = ["vote_presence"]
 
     def process(self):
         self.run_custom_validations()
@@ -36,17 +34,9 @@ class EventVoteServices(ServiceWithResult):
     @property
     @lru_cache
     def _get_selection(self):
-        try:
-            return Selection.objects.get(id=self.cleaned_data["id"])
-        except ObjectDoesNotExist:
-            return None
+        return Selection.objects.get(id=self.cleaned_data["id"])
 
     def vote_presence(self):
         if Vote.objects.filter(user=self.cleaned_data["user"], selection=self._get_selection).exists():
             raise ValidationError(message="Vote with this user and selection exists",
                                   response_status=status.HTTP_400_BAD_REQUEST)
-
-    def selection_presence(self):
-        if not self._get_selection:
-            raise NotFound(message=f"Selection with id {self.cleaned_data['id']} not found",
-                           response_status=status.HTTP_200_OK)
