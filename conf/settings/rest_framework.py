ded67654c42376cf2d54a81d6a3a67cbdf2eb1a3
SomeBody <maxim.karaichev@saltpepper.ru>
2023-02-23 14:11:33 +0300

    fix warning in drf

--- conf/settings/rest_framework.py
+++ conf/settings/rest_framework.py
@@ -5,6 +5,7 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.JSONParser",
         "rest_framework.parsers.MultiPartParser",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     "EXCEPTION_HANDLER": "utils.exception_handler.custom_exception_handler",
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
     "DEFAULT_AUTHENTICATION_CLASSES": ["api.authentication.UserApiTokenAuthentication"],
