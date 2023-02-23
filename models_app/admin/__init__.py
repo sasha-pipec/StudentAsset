e26b2a88933463efd3b5549b780bfd4a46bdcd68
SomeBody <maxim.karaichev@saltpepper.ru>
2023-02-23 16:42:49 +0300

    added posts in admin

--- models_app/admin/__init__.py
+++ models_app/admin/__init__.py
@@ -1,3 +1,5 @@
 from models_app.admin.vote.resources import VoteAdmin
 from models_app.admin.user.resources import UserAdmin
 from models_app.admin.event.resources import EventAdmin
+from models_app.admin.post.resources import PostAdmin
+
---
+++ models_app/admin/post/resources.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from models_app.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = [
+        "id",
+        "title",
+        "description",
+        "image",
+        "date",
+        "user",
+    ]
+    list_display = [
+        "id",
+        "title",
+        "date",
+        "image",
+        "user",
+    ]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    list_display_links = (
+        "id",
+        "title",
+    )
+    ordering = ("id", "user")
