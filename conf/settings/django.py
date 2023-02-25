a4f912f44434d73c152ca29c3c32ca8f8e50e014
SomeBody <maxim.karaichev@saltpepper.ru>
2023-02-25 22:35:40 +0300

    added cors in apps

--- conf/settings/django.py
+++ conf/settings/django.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     "drf_yasg",
     "rest_framework",
     'service_objects',
