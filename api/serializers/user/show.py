c015a3d7793d7f0e6b06e2860e314740a4f871a4
aleksandr razarenov <aleksandr.razarenov@saltpepper.ru>
2023-04-02 18:38:51 +0300

    fix user serialiser - add api key

--- api/serializers/user/show.py
+++ api/serializers/user/show.py
@@ -12,6 +12,7 @@ class UserShowSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             "id",
+            "API_Key",
             "username",
             "first_name",
             "last_name",
