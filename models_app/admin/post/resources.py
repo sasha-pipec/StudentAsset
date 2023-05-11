5c278794017250f7654df721622c02a23f4749d7
aleksandr razarenov <aleksandr.razarenov@saltpepper.ru>
2023-05-11 19:33:07 +0300

    fix post date more

--- models_app/admin/post/resources.py
+++ models_app/admin/post/resources.py
@@ -20,7 +20,7 @@ class PostAdmin(admin.ModelAdmin):
         "image",
         "user",
     ]
-    readonly_fields = ["id", "created_at", "updated_at", "date"]
+    readonly_fields = ["id", "created_at", "updated_at"]
     list_display_links = (
         "id",
         "title",
